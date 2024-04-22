0913b6ca29269b4929a8af05248fe01ce2999d18
Samuele-Benato <samuele.benato@gmail.com>
2024-04-22 15:09:11 +0200

    feat : add book class

---
+++ csharp-biblioteca/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Book : Document
+    {
+        public int Pages { get; set; }
+
+        public Book(string id, string title, int year, Sector sector, char shelf, byte position, string author, int pages) : base(id, title, year, sector, shelf, position, author)
+        {
+            Pages = pages;
+        }
+    }
+}
