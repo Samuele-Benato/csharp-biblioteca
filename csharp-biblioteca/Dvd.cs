9422bda1dbd04d6e8828e9d6a00678ab1a26085d
Samuele-Benato <samuele.benato@gmail.com>
2024-04-22 15:11:13 +0200

    feat : add dvd class

---
+++ csharp-biblioteca/Dvd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static csharp_biblioteca.Document;
+
+namespace csharp_biblioteca
+{
+    internal class Dvd : Document
+    {
+        public int Duration { get; set; }
+
+        public Dvd(string id, string title, int year, Sector sector, char shelf, byte position, string author, int duration) : base(id, title, year, sector, shelf, position, author)
+        {
+            Duration = duration;
+        }
+    }
+}
