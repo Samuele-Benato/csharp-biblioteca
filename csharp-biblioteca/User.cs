2cde08e9220a597f5ebb61cb9c73e990d68de660
Samuele-Benato <samuele.benato@gmail.com>
2024-04-22 14:19:32 +0200

    feat : add User class

---
+++ csharp-biblioteca/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class User
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        int Phone {  get; set; }
+
+        public User(string name, string surname , string email, string password , int phone)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+            Phone = phone;
+        }
+
+    }
+}
